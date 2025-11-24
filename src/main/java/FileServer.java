a4417ba08a0bfd24771eddac1f6fbb9fce7ceb5f
Kelles <95657019+K3ll3s@users.noreply.github.com>
2025-11-24 15:09:36 +0000

    demo cli FileServer.java
    - simple read-write capability

--- src/main/java/FileServer.java
+++ src/main/java/FileServer.java
@@ -1,22 +1,43 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
 
 public class FileServer {
 	public static void main(String[] args) {
 
-		int port = 8080;
-		try (ServerSocket serverSocket = new ServerSocket(port)) {
-			System.out.println("Server listening on localhost:8080");
+		int cliPort = 8080;
+		int filePort = 8081;
+		try (ServerSocket serverSocket = new ServerSocket(cliPort)) {
+
+			System.out.println("Server listening on localhost: " + cliPort);
 			while(true) {
 				Socket socket = serverSocket.accept();
-				BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
-				BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+				System.out.println("Client connected: " + socket.getInetAddress());
 
-				out.write("hello".getBytes());
-				out.flush();
+	            InputStreamReader isr = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+	            OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+	            BufferedReader br = new BufferedReader(isr);            
+	            BufferedWriter bw = new BufferedWriter(osw);
+
+				bw.write("Hello client");
+				bw.newLine();
+				bw.flush();
+				while (true) {
+					String response = br.readLine();
+					if (response.equals("LIST")){
+						bw.write("requested LIST command");
+						bw.newLine();
+						bw.flush();
+					} else {
+						System.out.println(response);
+					}
+				}
+
 			}
 		} catch (IOException e) {
 			System.out.println("Exception: " + e);
