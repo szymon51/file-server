9d30b3dc9d3936c10451de5fa32324ea4c77be6b
Kelles <95657019+K3ll3s@users.noreply.github.com>
2025-11-24 15:09:01 +0000

    demo cli FileClient.java
    - simple send-read capability

--- src/main/java/FileClient.java
+++ src/main/java/FileClient.java
@@ -1,28 +1,45 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
-
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class FileClient {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         String address = "localhost";
-        int port = 8080;
+        int cliPort = 8080;
+        int filePort = 8081;
 
-        try (Socket socket = new Socket(address, port)) {
+        try (Socket socket = new Socket(address, cliPort)) {
 
-            BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
-            BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+            OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(isr);            
+            BufferedWriter bw = new BufferedWriter(osw);
 
-            // Reading text data
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String line = reader.readLine();
-            System.out.println(line);
-          
-            socket.close();
+            System.out.println(br.readLine());
+
+            System.out.println("\nWelcome to cli interface, available commends are: LIST, GET, PUT, HELP");
+            while (true) {
+                String request = scanner.nextLine();
+                if (request.equals("LIST")){
+                    bw.write(request);
+                    bw.newLine();
+                    bw.flush();
+                    System.out.println(br.readLine());
+                } else {
+                    bw.write(request);
+                    bw.newLine();
+                    bw.flush();
+                }
+            }
+
         } catch (IOException e) {
             System.out.println("Exception: " + e);
         }
