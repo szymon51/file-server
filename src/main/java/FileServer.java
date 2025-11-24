28aaea5c7c3206fc9314361ce19e59248f8e86c9
Szymon51 <120176777+szymon51@users.noreply.github.com>
2025-11-24 20:07:59 +0100

    Refactor the main method, move the functionality to the start method

--- src/main/java/FileServer.java
+++ src/main/java/FileServer.java
@@ -9,27 +9,30 @@ import java.nio.charset.StandardCharsets;
 
 public class FileServer {
 	public static void main(String[] args) {
+		new FileServer().start();
+	}
 
+	public void start() {
 		int cliPort = 8080;
 		int filePort = 8081;
 		try (ServerSocket serverSocket = new ServerSocket(cliPort)) {
 
 			System.out.println("Server listening on localhost: " + cliPort);
-			while(true) {
+			while (true) {
 				Socket socket = serverSocket.accept();
 				System.out.println("Client connected: " + socket.getInetAddress());
 
-	            InputStreamReader isr = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
-	            OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
-	            BufferedReader br = new BufferedReader(isr);            
-	            BufferedWriter bw = new BufferedWriter(osw);
+				InputStreamReader isr = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+				OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+				BufferedReader br = new BufferedReader(isr);
+				BufferedWriter bw = new BufferedWriter(osw);
 
 				bw.write("Hello client");
 				bw.newLine();
 				bw.flush();
 				while (true) {
 					String response = br.readLine();
-					if (response.equals("LIST")){
+					if (response.equals("LIST")) {
 						bw.write("requested LIST command");
 						bw.newLine();
 						bw.flush();
